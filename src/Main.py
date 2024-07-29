802bb5840f8d587e8c8c454998911ea9bc42cb94
zs <zs@dasino.com>
2024-07-29 15:18:19 +0800

    update

--- src/Main.py
+++ src/Main.py
@@ -11,7 +11,8 @@ def lastPath(curAbsPath):
 
 
 def delPath(path):
-    shutil.rmtree(path, onerror=handle_remove_read_only)
+    if os.path.exists(path):
+        shutil.rmtree(path, onerror=handle_remove_read_only)
 
 
 def handle_remove_read_only(func, path, exc):
@@ -27,17 +28,50 @@ def nextPath(curAbsPath, next):
     return os.path.abspath(os.path.join(curAbsPath, next))
 
 
+def gitClone(repo, branch, name, path):
+    """
+    :param repo: 配置表仓库地址
+    :param branch: 所在分支
+    :param name: 克隆下来的本地目录
+    :param path: 在哪个文件夹下操作
+    """
+    repoPath = os.path.join(path, name)
+    if os.path.exists(repoPath) == False:
+        os.chdir(path)
+        os.system("git clone " + repo + " " + name)
+        print("git clone ok")
+        
+    os.chdir(repoPath)
+    print("git checkout " + branch)
+    os.system("git checkout " + branch)
+    print("checkout ok")
+    os.system("git pull --rebase")
+    print("pull ok")
+
+
 # 当前脚本执行所在的目录
 CurPath = os.path.abspath(os.path.join(sys.argv[0], ".."))
-print("CurPath", CurPath)
+print("当前路径", CurPath)
 
-# 删除所有.ts文件
-pattern = CurPath + "\*.ts"
-for ts in glob.glob(pattern, recursive = True):
-    os.remove(ts)
+# 更新仓库
+repo = "git@github.com:jfcwrlight/DasinoClient-Table.git"
+branch = "main"
+srcDirName = "tables"
+path = CurPath
+gitClone(repo, branch, srcDirName, path)
+
+# # 删除所有.ts文件
+# pattern = CurPath + "\*.ts"
+# for ts in glob.glob(pattern, recursive = True):
+#     os.remove(ts)
+
+# 删除输出目录
+ourDirPath = os.path.join(path, srcDirName, "out")
+delPath(ourDirPath)
 
 # 切换到工具脚本目录
 os.chdir(CurPath)
-os.system("python3 ExcelTo.py -t " + CurPath + " -o " + CurPath + " --ts")
+srcDirPath = os.path.join(CurPath, srcDirName)
+os.system("python3 ExcelTo.py -t " + srcDirPath + " -o " + ourDirPath + " --ts")
 
 print("-----------------打表完成-------------------") 
